d9a18e5f5ff9b77e1ff3f0dd3b89c3537fd1af13
vinod.p <vinod.pandella@moodle.com>
2024-11-11 12:45:28 +0530

    Zatuk Free

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -24,20 +24,103 @@
 
 namespace mod_zatuk\privacy;
 
+use core_privacy\local\metadata\collection;
+use core_privacy\local\request\approved_contextlist;
+use core_privacy\local\request\approved_userlist;
+use core_privacy\local\request\contextlist;
+use core_privacy\local\request\userlist;
+
 /**
  * The mod_zatuk module does not store any data.
  *
  * @copyright  2023 Moodle India
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\null_provider {
+class provider implements
+        // This plugin stores personal data.
+        \core_privacy\local\metadata\provider,
+
+        // This plugin is a core_user_data_provider.
+        \core_privacy\local\request\plugin\provider,
+
+        // This plugin is capable of determining which users have data within it.
+        \core_privacy\local\request\core_userlist_provider {
     /**
-     * Get the language string identifier with the component's language
-     * file to explain why this plugin stores no data.
+     * Return the fields which contain personal data.
      *
-     * @return  string
+     * @param collection $items a reference to the collection to use to store the metadata.
+     * @return collection the updated collection of metadata items.
      */
-    public static function get_reason(): string {
-        return 'privacy:metadata';
+    public static function get_metadata(collection $items): collection {
+        $items->add_database_table(
+            'zatuk_uploaded_videos',
+            [
+                'videoid' => 'privacy:metadata:zatuk_uploaded_videos:videoid',
+                'title' => 'privacy:metadata:zatuk_uploaded_videos:title',
+                'description' => 'privacy:metadata:zatuk_uploaded_videos:description',
+                'filename' => 'privacy:metadata:zatuk_uploaded_videos:filename',
+                'filepath' => 'privacy:metadata:zatuk_uploaded_videos:filepath',
+                'public' => 'privacy:metadata:zatuk_uploaded_videos:public',
+                'usercreated' => 'privacy:metadata:zatuk_uploaded_videos:usercreated',
+                'timecreated' => 'privacy:metadata:zatuk_uploaded_videos:timecreated',
+
+            ],
+            'privacy:metadata:zatuk_uploaded_videos'
+        );
+
+        return $items;
+    }
+    /**
+     * Get the list of contexts that contain user information for the specified user.
+     *
+     * @param int $userid the userid.
+     * @return contextlist the list of contexts containing user info for the user.
+     */
+    public static function get_contexts_for_userid(int $userid): contextlist {
+        return new contextlist();
+    }
+
+    /**
+     * Get the list of users who have data within a context.
+     *
+     * @param   userlist    $userlist   The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist) {
+
+    }
+
+    /**
+     * Export personal data for the given approved_contextlist. User and context information is contained within the contextlist.
+     *
+     * @param approved_contextlist $contextlist a list of contexts approved for export.
+     */
+    public static function export_user_data(approved_contextlist $contextlist) {
+
+    }
+
+    /**
+     * Delete all data for all users in the specified context.
+     *
+     * @param \context $context the context to delete in.
+     */
+    public static function delete_data_for_all_users_in_context(\context $context) {
+
+    }
+
+    /**
+     * Delete all user data for the specified user, in the specified contexts.
+     *
+     * @param approved_contextlist $contextlist a list of contexts approved for deletion.
+     */
+    public static function delete_data_for_user(approved_contextlist $contextlist) {
+
+    }
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param   approved_userlist       $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist) {
+
     }
 }
